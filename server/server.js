b07fcfe426fa4f9a222680a73eef1cecf3e82db3
Stycket <121405173+Stycket@users.noreply.github.com>
2023-10-17 04:58:56 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -134,7 +134,7 @@ removeDocumentsFromYesterday();
 
 // Schedule the task to run at 00:55 each day
 // Schedule the task to run at 01:05 each day in the Swedish timezone
-cron.schedule('8 3 * * *', () => {
+cron.schedule('2 5 * * *', () => {
   // Adjust the timezone to Europe/Stockholm (Swedish timezone)
   const swedishTimezone = 'Europe/Stockholm';
   const swedishNow = new Date().toLocaleString('en-US', { timeZone: swedishTimezone });
